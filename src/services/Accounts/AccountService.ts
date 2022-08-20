d4713d5080dbc848cd1419c5d1785b0b1039fe3c
chiro <jorge.del1991@yahoo.com.mx>
2022-08-20 11:29:37 -0500

    clean code

--- src/services/Accounts/AccountService.ts
+++ src/services/Accounts/AccountService.ts
@@ -1,10 +1,12 @@
 import {HostVariables} from "../../configs/HostVariables";
 
-    export async function postOpenAccount(accountName: string) {
+export async function postOpenAccount(accountName: string) {
     const response = await fetch(HostVariables.baseApiURL + "/Accounts/Open", {
         method: "POST",
-        headers: {'Content-Type': 'application/json', 
-            'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJUZXN0SXNzdWVyIiwiaWF0IjoxNjU4MTAxNzIxLCJleHAiOjE2ODk2Mzc3MjEsImF1ZCI6Ind3dy5leGFtcGxlLmNvbSIsInN1YiI6Impyb2NrZXRAZXhhbXBsZS5jb20iLCJHaXZlbk5hbWUiOiJKb2hubnkifQ._tGy-Rh1FpeH1PjuySi4Lh5yyetCMPkhClaBFGZxdtI'},
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJUZXN0SXNzdWVyIiwiaWF0IjoxNjU4MTAxNzIxLCJleHAiOjE2ODk2Mzc3MjEsImF1ZCI6Ind3dy5leGFtcGxlLmNvbSIsInN1YiI6Impyb2NrZXRAZXhhbXBsZS5jb20iLCJHaXZlbk5hbWUiOiJKb2hubnkifQ._tGy-Rh1FpeH1PjuySi4Lh5yyetCMPkhClaBFGZxdtI'
+        },
         body: JSON.stringify({AccountName: accountName})
     });
     
