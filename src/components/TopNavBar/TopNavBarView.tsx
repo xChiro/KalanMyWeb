a5e5163a5c7303355049b2bd0bbeef3b2935c9fd
chiro <jorge.del1991@yahoo.com.mx>
2022-08-31 08:09:41 -0500

    remove unused variable

--- src/components/TopNavBar/TopNavBarView.tsx
+++ src/components/TopNavBar/TopNavBarView.tsx
@@ -4,7 +4,7 @@ import {useAuth0} from "@auth0/auth0-react";
 import {Button} from "react-bootstrap";
 
 function TopNavBarView() {
-    const {logout, user} = useAuth0();
+    const {logout} = useAuth0();
 
     return (
         <div className="container-fluid" style={{
